cf3515e42219e0aa9fb0b091813a8e5b08c68e1a
Harpreet <harpreetseera8525@gmail.com>
2023-08-11 15:18:08 +0530

    minor fixes

--- lib/ui/widgets/timer_with_actions.dart
+++ lib/ui/widgets/timer_with_actions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:ipotato_timer/modal/task_data.dart';
 import 'package:ipotato_timer/repository/database/database_interface.dart';
 import 'package:ipotato_timer/ui/widgets/timer_action_button.dart';
@@ -14,35 +15,37 @@ class TimerWithActions extends StatelessWidget {
   get genericVerticalSpace => const SizedBox(height: 8);
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Text(
-          Utility.resolveTimer(taskData.duration),
-          style: Theme.of(context)
-              .textTheme
-              .headlineLarge!
-              .copyWith(color: Theme.of(context).colorScheme.primary),
-        ),
-        genericHorizontalSpace,
-        TimerActionButton(
-          iconData: taskData.isActive ? Icons.pause : Icons.play_arrow,
-          action: () {
-            taskData.isActive = !taskData.isActive;
-            if (taskData.isActive) {
-              taskData.decrement();
-            }
-            taskData.registerTime = DateTime.now();
-            context.read<IPotatoTimerDB>().updateTaskInDB(taskData);
-          },
-        ),
-        genericHorizontalSpace,
-        TimerActionButton(
-            iconData: Icons.stop_rounded,
+    return Observer(
+      builder: (_) => Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            Utility.resolveTimer(taskData.duration),
+            style: Theme.of(context)
+                .textTheme
+                .headlineLarge!
+                .copyWith(color: Theme.of(context).colorScheme.primary),
+          ),
+          genericHorizontalSpace,
+          TimerActionButton(
+            iconData: taskData.isActive ? Icons.pause : Icons.play_arrow,
             action: () {
-              deleteTask(context, taskData);
-            }),
-      ],
+              taskData.isActive = !taskData.isActive;
+              if (taskData.isActive) {
+                taskData.decrement();
+              }
+              taskData.registerTime = DateTime.now();
+              context.read<IPotatoTimerDB>().updateTaskInDB(taskData);
+            },
+          ),
+          genericHorizontalSpace,
+          TimerActionButton(
+              iconData: Icons.stop_rounded,
+              action: () {
+                deleteTask(context, taskData);
+              }),
+        ],
+      ),
     );
   }
 }
