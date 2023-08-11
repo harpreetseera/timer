540924fd113fd3c50a2feedd40640dc9c9722159
Harpreet <harpreetseera8525@gmail.com>
2023-08-12 01:29:28 +0530

    minor fixes

--- test/int_extension_test.dart
+++ test/int_extension_test.dart
@@ -1,30 +1,28 @@
 import 'package:test/test.dart';
 import 'package:ipotato_timer/extension/int_extension.dart';
 
-void main(){
-  
-  group('IntExtension',(){
-     test("zero should be prefixed",(){
-    final duration = 0;
-    final valueAfterPrefix = duration.prefixZeroForSingleDigit();
-    expect(valueAfterPrefix,"00");
-  });
+void main() {
+  group('IntExtension', () {
+    test("zero should be prefixed", () {
+      const duration = 0;
+      final valueAfterPrefix = duration.prefixZeroForSingleDigit();
+      expect(valueAfterPrefix, "00");
+    });
 
-   test("zero should be prefixed edge case - 9",(){
-    final duration = 9;
-    final valueAfterPrefix = duration.prefixZeroForSingleDigit();
-    expect(valueAfterPrefix,"09");
-  }); 
-  test("zero should not be prefixed",(){
-     final duration = 10;
-    final valueAfterPrefix = duration.prefixZeroForSingleDigit();
-    expect(valueAfterPrefix,"10");
-  });
-  test("zero should not be added for value less than 0",(){
-     final duration = -1;
-    final valueAfterPrefix = duration.prefixZeroForSingleDigit();
-    expect(valueAfterPrefix,"-1");
-  });
-
+    test("zero should be prefixed edge case - 9", () {
+      const duration = 9;
+      final valueAfterPrefix = duration.prefixZeroForSingleDigit();
+      expect(valueAfterPrefix, "09");
+    });
+    test("zero should not be prefixed", () {
+      const duration = 10;
+      final valueAfterPrefix = duration.prefixZeroForSingleDigit();
+      expect(valueAfterPrefix, "10");
+    });
+    test("zero should not be added for value less than 0", () {
+      const duration = -1;
+      final valueAfterPrefix = duration.prefixZeroForSingleDigit();
+      expect(valueAfterPrefix, "-1");
+    });
   });
 }
