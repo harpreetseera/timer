cfe2f54243f63bebfe98f1518ce06013d0e5e1cb
Harpreet <harpreetseera8525@gmail.com>
2023-08-12 16:50:33 +0530

    comments int ext

--- lib/extension/int_extension.dart
+++ lib/extension/int_extension.dart
@@ -1,8 +1,12 @@
+///Extension over int
 extension IntExtension on int {
+  /// prefixes a zero above an integer in the range of 0-9
+  /// and returns as a String
   String prefixZeroForSingleDigit() {
     if (this >= 0 && this < 10) {
       return "0$this";
     } else {
+      // Since the value is 2 digit so the value is returned as it is
       return toString();
     }
   }
